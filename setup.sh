290574c974be1d13994baeb860831ad2a0233be5
luis miguel yances castillo <Luisyances1@gmail.com>
2022-02-19 05:47:09 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 mkdir -p ~ /.streamlit/
 echo  "
-[theme]
-base='dark'
-primaryColor='#4b64ff'
-secondaryBackgroundColor= '# 2c2c2d'
-font = 'monoespace'
 [server]
 headless = true
 enableCORS=false
